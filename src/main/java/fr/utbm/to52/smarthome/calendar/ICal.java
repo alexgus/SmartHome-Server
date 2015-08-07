7ea4c1b1550695fa8eaddf61b580848a916b10c1
alexgus <alex.guyon78@gmail.com>
2015-08-07 20:12:39 +0200

    Added get method for ical

--- src/main/java/fr/utbm/to52/smarthome/calendar/ICal.java
+++ src/main/java/fr/utbm/to52/smarthome/calendar/ICal.java
@@ -4,6 +4,7 @@
 package fr.utbm.to52.smarthome.calendar;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
 import java.util.UUID;
@@ -11,7 +12,11 @@ import java.util.UUID;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.filter.Filter;
 import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.ComponentList;
+import net.fortuna.ical4j.model.Property;
+import net.fortuna.ical4j.model.property.DtEnd;
+import net.fortuna.ical4j.model.property.DtStart;
 import net.fortuna.ical4j.util.Calendars;
 
 /**
@@ -30,6 +35,31 @@ public class ICal implements ICalendar {
 	 */
 	public ICal(URL u){
 		this.url = u;
+	}
+	
+	/**
+	 * Reception of test calendar
+	 * @param arg Not used
+	 */
+	public static void main(String[] arg){
+		ICal c;
+		try {
+			c = new ICal(new URL("https://www.google.com/calendar/ical/alex.guyon78%40gmail.com/public/basic.ics"));
+			c.load();
+			
+			
+			java.util.Calendar calToday = java.util.Calendar.getInstance();
+			java.util.Calendar calMonthMinus1 = java.util.Calendar.getInstance();
+			calMonthMinus1.set(java.util.Calendar.MONTH, 
+					calToday.get(java.util.Calendar.MONTH) - 1); 
+			
+			ComponentList lc = c.get(calMonthMinus1.getTime(), calToday.getTime());
+			
+			System.out.println(lc);
+			
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}		
 	}
 	
 	/* (non-Javadoc)
@@ -93,9 +123,28 @@ public class ICal implements ICalendar {
 	 * @see fr.utbm.to52.smarthome.calendar.ICalendar#get(java.util.Date, java.util.Date)
 	 */
 	@Override
-	public ComponentList get(Date begin, Date end) {
-		// TODO Auto-generated method stub
-		return null;
+	public ComponentList get(Date begin, Date end) {		
+		ComponentList cFiltered = new ComponentList();
+		ComponentList cAll = this.cal.getComponents().getComponents(Component.VEVENT);
+		
+		for (Object o : cAll) {
+			Component c = (Component) o;
+			
+			Property p1 = c.getProperty(Property.DTSTART);
+			DtStart dBegin = (DtStart) p1;
+			
+			Property p2 = c.getProperty(Property.DTEND);
+			DtEnd dEnd = (DtEnd) p2;
+			
+			boolean part1 = dBegin.getDate().after(begin);
+			boolean part2 = dBegin.getDate().before(end);
+			
+			if(part1 && part2
+					/*&& dEnd.getDate().before(end) && dEnd.getDate().after(begin)*/)
+				cFiltered.add(c);
+		}
+		
+		return cFiltered;
 	}
 
 	/* (non-Javadoc)
@@ -112,7 +161,9 @@ public class ICal implements ICalendar {
 	 */
 	@Override
 	public String toICal() {
-		return this.cal.toString();
+		if(this.cal != null)
+			return this.cal.toString();
+		return null;
 	}
 
 	/**
