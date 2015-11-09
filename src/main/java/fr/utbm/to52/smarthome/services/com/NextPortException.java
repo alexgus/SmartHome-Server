d5cc3372e5f6b97def8d7285cd30da0b42446ade
alexgus <alex.guyon78@gmail.com>
2015-11-09 18:49:09 +0100

    Added comments

--- src/main/java/fr/utbm/to52/smarthome/services/com/NextPortException.java
+++ src/main/java/fr/utbm/to52/smarthome/services/com/NextPortException.java
@@ -17,14 +17,15 @@ public class NextPortException extends Exception {
 	private int port;
 	
 	/**
-	 * 
+	 * @param port The next port choosen
 	 */
 	public NextPortException(int port) {
 		this.port = port;
 	}
 
 	/**
-	 * @param message
+	 * @param message message of the exception
+	 * @param port The next port chosen
 	 */
 	public NextPortException(String message, int port) {
 		super(message);
@@ -32,7 +33,8 @@ public class NextPortException extends Exception {
 	}
 
 	/**
-	 * @param cause
+	 * @param cause cause of the exception
+	 * @param port The next port chosen
 	 */
 	public NextPortException(Throwable cause, int port) {
 		super(cause);
@@ -40,8 +42,9 @@ public class NextPortException extends Exception {
 	}
 
 	/**
-	 * @param message
-	 * @param cause
+	 * @param message message of the exception
+	 * @param cause cause of the exception
+	 * @param port The next port chosen 
 	 */
 	public NextPortException(String message, Throwable cause, int port) {
 		super(message, cause);
@@ -49,16 +52,20 @@ public class NextPortException extends Exception {
 	}
 
 	/**
-	 * @param message
-	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
+	 * @param message message of the exception
+	 * @param cause cause of the exception
+	 * @param enableSuppression ?
+	 * @param writableStackTrace ?
+	 * @param port The next port chosen
 	 */
 	public NextPortException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, int port) {
 		super(message, cause, enableSuppression, writableStackTrace);
 		this.port = port;
 	}
 	
+	/**
+	 * @return The next port chosen
+	 */
 	public int getPort(){
 		return this.port;
 	}
