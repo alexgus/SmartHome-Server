03fb64418ca78a739e53332050cf678e998a2a0c
alexgus <alex.guyon78@gmail.com>
2015-11-09 18:23:51 +0100

    Added basic notes

---
+++ src/main/java/fr/utbm/to52/smarthome/model/note/Tag.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package fr.utbm.to52.smarthome.model.note;
+
+/**
+ * @author Alexandre Guyon
+ *
+ */
+public class Tag {
+
+	private String tag;
+	
+	/**
+	 * @param tag Tag to create
+	 */
+	public Tag(String tag) {
+		this.tag = tag;
+	}
+
+	/**
+	 * @return the tag
+	 */
+	public String getTag() {
+		return this.tag;
+	}
+
+	/**
+	 * @param tag the tag to set
+	 */
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.tag == null) ? 0 : this.tag.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		if (this.tag == null) {
+			if (other.tag != null)
+				return false;
+		} else if (!this.tag.equals(other.tag))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Tag [tag=" + this.tag + "]";
+	}
+
+}
