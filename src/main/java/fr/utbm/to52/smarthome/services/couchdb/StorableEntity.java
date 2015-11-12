17735ec3a44c3e1af2046b49a5081924504778ab
alexgus <alex.guyon78@gmail.com>
2015-11-12 10:51:46 +0100

    Override basic method for StorableEntiy

--- src/main/java/fr/utbm/to52/smarthome/services/couchdb/StorableEntity.java
+++ src/main/java/fr/utbm/to52/smarthome/services/couchdb/StorableEntity.java
@@ -22,4 +22,43 @@ public abstract class StorableEntity {
 		this.$table = table;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.$table == null) ? 0 : this.$table.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StorableEntity other = (StorableEntity) obj;
+		if (this.$table == null) {
+			if (other.$table != null)
+				return false;
+		} else if (!this.$table.equals(other.$table))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "StorableEntity [$table=" + this.$table + "]";
+	}
+
 }
