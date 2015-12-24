1b566163ef9d2780e9ca8c916cfcc017d47af22f
alexgus <alex.guyon78@gmail.com>
2015-12-24 17:59:10 +0100

    Added madkit

---
+++ src/main/java/fr/utbm/to52/smarthome/controller/agent/ComponentAgent.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package fr.utbm.to52.smarthome.controller.agent;
+
+import madkit.kernel.Agent;
+
+/**
+ * @author Alexandre Guyon
+ *
+ */
+public class ComponentAgent extends Agent {
+
+}
